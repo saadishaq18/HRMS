928c373932bb61355eef50fec4b919580e2715dc
Saad Ishaq <Saad.Ishaq@sapphirecs.net>
2024-03-26 10:45:08 +0500

    Created model of salary

--- model/employeeSalaryModel.js
+++ model/employeeSalaryModel.js
@@ -1,1 +1,61 @@
+const mongoose = require('mongoose')
+const salarySchema = new mongoose.Schema({
+    user_id: {
+        type:Number,
+        required: true
+    },
+    net_salary: {
+        type: Number,
+        required: true
+    },
+    baisc_earning:{
+        type: Number,
+        required: true
+    },
+    da_earning:{
+        type:Number
+    },
+    hra_earning:{
+        type: Number
+    },
+    conveyance_earning:{
+        type: Number
+    },
+    allowance_earning:{
+        type: Number
+    },
+    medical_allowance_earning:{
+        type: Number
+    },
+    other_earning:{
+        type:Number
+    },
+    tds_deduction:{
+        type: Number
+    },
+    esi_deduction:{
+        type: Number
+    },
+    pf_deduction:{
+        type: Number
+    },
+    leave_deduction:{
+        type: Number
+    },
+    prof_tax_deduction: {
+        type: Number
+    },
+    labour_welfare_deduction: {
+        type: Number
+    },
+    other_deduction: {
+        type: Number
+    }
 
+    
+
+
+})
+const SALARY = mongoose.model("salary", salarySchema)
+
+module.exports = SALARY
